16a2f4a73a3136880a12cf7303855fe777ce3798
Jack <jack.gardner@oneflowsystems.com>
2015-06-16 10:49:14 +0100

    Upload fixes

--- dist/image-upload.js
+++ dist/image-upload.js
@@ -145,32 +145,20 @@ angular.module('oneflow.image-upload').run(['$templateCache', function($template
   'use strict';
 
   $templateCache.put('templates/image-upload.html',
-    "<div>\r" +
-    "\n" +
-    "  <ul class=\"image-list\">\r" +
-    "\n" +
-    "    <li ng-repeat=\"image in ngModel\">\r" +
-    "\n" +
-    "      <img ng-click=\"removeImage($index)\" ng-src=\"{{image}}\" />\r" +
-    "\n" +
-    "    </li>\r" +
-    "\n" +
-    "  </ul>\r" +
-    "\n" +
-    "  <div class=\"clearfix m-b\"></div>\r" +
-    "\n" +
-    "  <input type=\"file\" upload-file-id=\"image-{{model._id}}\" id=\"image-{{model._id}}\" onchange=\"angular.element(this).scope().onFileSelect(this)\" />\r" +
-    "\n" +
-    "  <div class=\"m-t\">\r" +
-    "\n" +
-    "    <img ng-show=\"selectedImage\" style=\"height: 200px;\" id=\"selectedimage\" ng-src=\"{{selectedImage}}\" name=\"selectedImage\" />\r" +
-    "\n" +
-    "  </div>\r" +
-    "\n" +
-    "  <button class=\"m-t btn btn-success\" translate=\"Add Image to {{type}}\" ng-click=\"addProductImage();$event.preventDefault()\">Add Image to {{type}}</button>\r" +
-    "\n" +
-    "</div>\r" +
-    "\n"
+    "<div>\n" +
+    "  <ul class=\"image-list\">\n" +
+    "    <li ng-repeat=\"image in ngModel\">\n" +
+    "      <img  ng-src=\"{{image}}\" class=\"image-upload\" />\n" +
+    "      <span ng-click=\"removeImage($index)\" class=\"image-text\"><span>Remove</span></span>\n" +
+    "    </li>\n" +
+    "  </ul>\n" +
+    "  <div class=\"clearfix m-b\"></div>\n" +
+    "  <input type=\"file\" upload-file-id=\"image-{{model._id}}\" id=\"image-{{model._id}}\" onchange=\"angular.element(this).scope().onFileSelect(this)\" />\n" +
+    "  <div class=\"m-t\">\n" +
+    "    <img ng-show=\"selectedImage\" style=\"height: 200px;\" id=\"selectedimage\" ng-src=\"{{selectedImage}}\" name=\"selectedImage\" />\n" +
+    "  </div>\n" +
+    "  <button class=\"m-t btn btn-success\" translate=\"Add Image to {{type}}\" ng-click=\"addProductImage();$event.preventDefault()\">Add Image to {{type}}</button>\n" +
+    "</div>\n"
   );
 
 }]);
